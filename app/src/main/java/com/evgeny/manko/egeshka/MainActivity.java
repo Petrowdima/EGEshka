79ef4ea0225e62ac4d2a9c6ba5a6c29f235a7a0f
Евгений Манько < >
2016-01-08 20:16:55 +0300

    Test Activity

--- app/src/main/java/com/evgeny/manko/egeshka/MainActivity.java
+++ app/src/main/java/com/evgeny/manko/egeshka/MainActivity.java
@@ -1,37 +1,46 @@
 package com.evgeny.manko.egeshka;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    Button bMathBtn;
-    Button rusBtn;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button btnMath;
+    Button btnRus;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bMathBtn = (Button) findViewById(R.id.bMathBtn);
-        rusBtn = (Button) findViewById(R.id.RusBtn);
-        bMathBtn.setOnClickListener(this);
-        rusBtn.setOnClickListener(this);
-    }
 
-    @Override
-    public void onClick(View v) {
-        Intent intent;
-        switch(v.getId()){
-            case R.id.bMathBtn:
-                intent = new Intent(this, basicMathActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.RusBtn:
-                intent = new Intent(this, RussianActivity.class);
-                startActivity(intent);
-                break;
-        }
+        btnMath = (Button) findViewById(R.id.btnMath);
+        btnRus = (Button) findViewById(R.id.btnRus);
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.btnMath:
+                        break;
+                    case R.id.btnRus:
+                        Intent intent = new Intent(getApplicationContext(), rus.class);
+                        startActivity(intent);
+                        break;
+
+                }
+            }
+        };
+        btnMath.setOnClickListener(onClickListener);
+        btnRus.setOnClickListener(onClickListener);
     }
 }
+
+
+
