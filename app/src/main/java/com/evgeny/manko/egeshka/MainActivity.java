fb04899f0b9cbb73eac52153e0fd11c5efd7d937
RomanKorolev <roman-korolev2007@narod.ru>
2016-01-10 03:59:22 +0400

    Проблемный MainActivity.java версия из репозитария очень сильно отличается от версии Даниила

--- app/src/main/java/com/evgeny/manko/egeshka/MainActivity.java
+++ app/src/main/java/com/evgeny/manko/egeshka/MainActivity.java
@@ -1,3 +1,4 @@
+//Копия файла из репозитария
 package com.evgeny.manko.egeshka;
 
 import android.content.Intent;
@@ -43,4 +44,43 @@ public class MainActivity extends AppCompatActivity {
 }
 
 
+/*
+//Копия файла присланная мне Даниилом https://vk.com/danek130995 danek130995@github
+package com.evgeny.manko.egeshka;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button bMathBtn;
+    Button rusBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bMathBtn = (Button) findViewById(R.id.bMathBtn);
+        rusBtn = (Button) findViewById(R.id.RusBtn);
+        bMathBtn.setOnClickListener(this);
+        rusBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch(v.getId()){
+            case R.id.bMathBtn:
+                intent = new Intent(this, basicMathActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.RusBtn:
+                intent = new Intent(this, RussianActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+}
+*/
